dd96c34f2229da2cdc69141736b99626a6c632d6
Remy Liu <36778645+RemyLau@users.noreply.github.com>
2022-09-07 10:40:18 -0400

    feat: new random perturbations (#1)
    - Random node features
    - Random edge rewiring

--- gtaxogym/transform/perturbations/__init__.py
+++ gtaxogym/transform/perturbations/__init__.py
@@ -6,7 +6,8 @@ from torch_geometric.graphgym.config import cfg
 from gtaxogym.utils import timeit
 from gtaxogym.transform.transforms import pre_transform_in_memory
 from gtaxogym.transform.perturbations.standard import \
-    Fragmented, FullyConnected, NodeDegree, NoEdges, NoFeatures
+    Fragmented, FullyConnected, NodeDegree, NoEdges, NoFeatures, \
+    RandomEdgeRewire, RandomNodeFeatures
 from gtaxogym.transform.perturbations.spectral import \
     BandpassFiltering, ClusterSparsification, FiedlerFragmentation, \
     WaveletBankFiltering
@@ -16,9 +17,11 @@ __all__ = [
     'FiedlerFragmentation',
     'Fragmented',
     'FullyConnected',
-    'NodeDegree',
     'NoEdges',
     'NoFeatures',
+    'NodeDegree',
+    'RandomEdgeRewire',
+    'RandomNodeFeatures',
     'WaveletBankFiltering',
 ]
 
@@ -49,16 +52,23 @@ def perturb_dataset(dataset):
             fn = NoEdges()
         else:
             fn = FullyConnected()
-    elif ptype == 'NodeDegree':
-        fn = NodeDegree(dataset, cfg.dataset.transductive)
     elif ptype == 'NoEdges':
         fn = NoEdges()
+    elif ptype == 'NodeDegree':
+        fn = NodeDegree(dataset, cfg.dataset.transductive)
     elif ptype == 'NoFeatures':
         fn = NoFeatures()
     elif ptype == 'NoFeaturesNoEdges':
         fn = Compose([NoEdges(), NoFeatures()])
     elif ptype == 'NoFeaturesFragk1':
         fn = Compose([NoFeatures(), Fragmented(k=1)])
+    elif ptype == 'RandomNodeFeatures':
+        fn = RandomNodeFeatures(cfg.perturbation.RandomNodeFeatures_feat_dim,
+                                cfg.perturbation.RandomNodeFeatures_min_val,
+                                cfg.perturbation.RandomNodeFeatures_max_val)
+    elif ptype == 'RandomEdgeRewire':
+        fn = RandomEdgeRewire(cfg.perturbation.RandomEdgeRewire_rewire_ratio,
+                              cfg.perturbation.RandomEdgeRewire_with_replacement)
     elif ptype.startswith('WaveletBankFiltering'):
         bands = None
         if ptype.endswith('-lo'):
